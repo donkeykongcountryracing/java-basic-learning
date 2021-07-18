299f9f569ad3e316c76889bd7ad7c5c03f71ab1b
Ethan  Jin <ethanthefatcow@gmail.com>
2021-07-18 17:11:03 +0800

    this is the division and showinf oof 3, 5, and 15. It uses arrays and loops. review this(it is hard)

--- src/DivideByThreeFive.java
+++ src/DivideByThreeFive.java
@@ -1,13 +1,16 @@
 public class DivideByThreeFive {
     public static void main(String[] args) {
+        //this is an array and this is the amount of multiples in three five and 15
         int[] multipleOfThree = new int[33];
         int[] multipleOfFive = new int[20];
         int[] multipleOfThreeAndFive = new int[6];
+        //this is the index of each of the multiples in the arrays above
 
         int index3 = 0;
         int index5 = 0;
         int index3and5 = 0;
-
+        //i used multiple ifs because if i use if else it would be connected to the first if, for example, if fifteen was x then it would be
+        //only go to index and if else would not count. so i made the ifs sepearate so ic an track each integer individually
         for (int x = 1; x <= 100; x++) {
             if (x % 3 == 0) {
                 multipleOfThree[index3] = x;
@@ -23,20 +26,20 @@ public class DivideByThreeFive {
             }
         }
 
-        System.out.println("divided by 3: " );
+        System.out.println("divided by 3: ");
         for (int i = 0; i < multipleOfThree.length; i++) {
-            System.out.print(i + ", ");
+            System.out.print(multipleOfThree[i] + ", ");// the bracket in the array shows the position of each number and the value that corresponds with it
         }
         System.out.println();
-            System.out.println("divided by 5: " );
-            for (int b = 0; b < multipleOfFive.length; b++) {
-                System.out.print(b + ", ");
-            }
+        System.out.println("divided by 5: ");
+        for (int b = 0; b < multipleOfFive.length; b++) {
+            System.out.print(multipleOfFive[b] + ", ");
+        }
         System.out.println();
-                System.out.println("divided by 3 and 5: " );
-                for (int c = 0; c < multipleOfThreeAndFive.length; c++) {
-                    System.out.print(c + ", ");
-                }
+        System.out.println("divided by 3 and 5: ");
+        for (int c = 0; c < multipleOfThreeAndFive.length; c++) {
+            System.out.print(multipleOfThreeAndFive[c] + ", ");
+        }
 
-            }
-        }
+    }
+}
